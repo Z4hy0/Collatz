5d4e8445746ae6e3aa71f9f9283c783c1891fa92
Francisco Nailton Arruda Machado <129246640+Z4hy0@users.noreply.github.com>
2024-09-19 12:47:00 -0300

    Update Collatz.py
    Adicionado alguns comentários

--- Collatz.py
+++ Collatz.py
@@ -1,12 +1,13 @@
+# Loop para que um número natural positivo seja inserido
 while True:
     c0 = int(input("Digite um número"))
-    if c0 > 0:
+    if c0 > 0: # Sai do loop se o número for positivo
         break
     else:
         print("Erro! Digite um número natural positivo maior que zero")
-steps = 0  # Contagem das etapas
+steps = 0  # Contador de etapas
 
-while c0 != 1:  # Loop while até que c0 seja igual a 1
+while c0 != 1:  # Executa o loop até que c0 seja igual a 1
     print(f"O valor atual de c0: {c0}")
     if c0 % 2 == 0:  # Descobrindo se o c0 é par, Se a diferença de c0 dividido por 2 não for 0, o número é impar
 
@@ -15,5 +16,6 @@ while c0 != 1:  # Loop while até que c0 seja igual a 1
         c0 = 3 * c0 + 1  # Se for impar, multiplica por 3 e soma +1
 
     steps += 1  # Incrementa +1 na contagem de etapas
+    
     print(f"O valor Final de c0: {c0}")  # Imprime o c0 quando ele for 1
     print("Número de steps", steps)  # Imprime o número de etapas
